07d24ed19170338f3182fac6f641ff61d19e243a
VikaChubykina <vtubukina90@gmail.com>
2025-10-05 19:08:01 +0300

    add tests in UnitTest1.cs

--- EdTechApp.Tests/UnitTest1.cs
+++ EdTechApp.Tests/UnitTest1.cs
@@ -1,10 +1,40 @@
-namespace EdTechApp.Tests;
+using Xunit;
+using EdTechApp; // щоб бачити класи з основного проєкту
+using System.Linq;
+using EdTechApp.Models;
+using EdTechApp.Services;
 
-public class UnitTest1
+
+namespace EdTechApp.Tests
 {
-    [Fact]
-    public void Test1()
+    public class StudentTests
     {
+        [Fact] // атрибут означає "це тест"
+        public void Student_Can_Enroll_In_Course()
+        {
+            // Arrange (підготовка)
+            var student = new Student("Іван", "ivan@test.com", 20, "Група A");
+            var teacher = new Teacher("Петро", "petro@test.com");
+            var course = teacher.CreateCourse("Математика", "Алгебра і геометрія", true);
 
+            // Act (дія)
+            student.Enroll(course);
+
+            // Assert (перевірка результату)
+            Assert.Contains(course, student.EnrolledCourses);
+        }
+
+        [Fact]
+        public void Assignment_Marked_As_Completed()
+        {
+            // Arrange
+            var assignment = new Assignment("Домашка №1", "Розв’язати рівняння");
+
+            // Act
+            assignment.Complete();
+
+            // Assert
+            Assert.True(assignment.IsCompleted);
+        }
     }
 }
